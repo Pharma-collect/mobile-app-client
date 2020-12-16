4496bc528d9f605440463dedca13ec888f574725
melvinmarra <melvin.marra@gmail.com>
2020-12-16 15:48:33 +0100

    Maps finished (Localisation / Nearby pharma / Permission)

--- app/src/main/java/projetbe/romelemma/ui/map/MapFragment.kt
+++ app/src/main/java/projetbe/romelemma/ui/map/MapFragment.kt
@@ -1,8 +1,10 @@
 package projetbe.romelemma.ui.map
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
@@ -10,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -20,20 +23,21 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-
 import projetbe.romelemma.R
 import projetbe.romelemma.dataClass.RootObject
 import projetbe.romelemma.services.GoogleApiService
 import projetbe.romelemma.services.IGoogleApiService
-
+import projetbe.romelemma.services.PermissionService
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
 
-
+    private val REQUEST_CODE_LOCATION_PERMISSION = 0
     private lateinit var mMap: GoogleMap
     private lateinit var mLastLocation: Location
 
@@ -54,7 +58,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private var mapReady = false
 
 
-
     @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -65,6 +68,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             ViewModelProvider(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.map_fragment, container, false)
 
+        requestPermission()
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -76,44 +80,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
 
-        nearbyPlace()
 
         return root
     }
 
-    fun nearbyPlace(){
-        //mMap.clear()
-        val url = getUrl(latitude, longitude)
-        Log.d("URL", url)
-        mService.getNearbyPlaces(url)
-            .enqueue(object : Callback<RootObject> {
-                override fun onResponse(call: Call<RootObject>?, response: Response<RootObject>?) {
-                    currentPlace = response!!.body()!!
-                    if(response!!.isSuccessful){
-                        for(i in 0 until response.body()!!.results!!.size){
-                            val markerOptions = MarkerOptions()
-                            val googlePlace = response.body()!!.results!![i]
-                            val lat = googlePlace.geometry!!.location!!.lat
-                            val lng = googlePlace.geometry!!.location!!.lng
-                            val placeName = googlePlace.name
-                            val latLng = LatLng(lat, lng)
-
-                            markerOptions.position(latLng)
-                            markerOptions.title(placeName)
-                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                            markerOptions.snippet(i.toString())
-
-                            mMap.addMarker(markerOptions)
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-                            mMap.animateCamera(CameraUpdateFactory.zoomTo(13f))
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<RootObject>, t: Throwable) {
-                }
-            })
-    }
 
     fun getUrl(latitude: Double, longitude: Double): String {
         Log.d("Latitude getUrl", latitude.toString())
@@ -210,4 +180,46 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    private fun requestPermission() {
+        if(PermissionService.hasLocationPermissions(requireContext())){
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept permissions to use this",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept permissions to use this",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
--- app/src/main/java/projetbe/romelemma/ui/map/MapViewModel.kt
+++ app/src/main/java/projetbe/romelemma/ui/map/MapViewModel.kt
@@ -1,37 +1,11 @@
 package projetbe.romelemma.ui.map
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import projetbe.romelemma.R
-import projetbe.romelemma.dataClass.RootObject
-import projetbe.romelemma.services.IGoogleApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MapViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is Map Fragment"
 
     }
-
-    val text: LiveData<String> = _text
-    private lateinit var mMap: GoogleMap
-
-    private var latitude: Double=0.toDouble()
-    private var longitude: Double=0.toDouble()
-
-    lateinit var mService: IGoogleApiService
-
-    internal lateinit var currentPlace:RootObject
-
-
-
-
 }
