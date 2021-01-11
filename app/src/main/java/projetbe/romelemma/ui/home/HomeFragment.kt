749732a61201145a201153ee60b13fb3353e8e5e
emmacourreges <emma.courreges@isen.yncrea.fr>
2021-01-11 15:31:33 +0100

    Design Home en cours

--- app/src/main/java/projetbe/romelemma/ui/home/HomeFragment.kt
+++ app/src/main/java/projetbe/romelemma/ui/home/HomeFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import projetbe.romelemma.R
+import projetbe.romelemma.ui.shop.AccueilShopFragment
 
 class HomeFragment : Fragment(){
 
@@ -24,6 +26,16 @@ class HomeFragment : Fragment(){
         homeViewModel.welcomeText.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val button_shop: Button = root.findViewById(R.id.btnShop)
+
+        button_shop.setOnClickListener {
+            //shop.visibility = View.INVISIBLE;
+            val fragment1: Fragment =
+                AccueilShopFragment();
+            val transaction = childFragmentManager.beginTransaction()
+            transaction.replace(R.id.fr_home, fragment1)
+            transaction.commit()        }
         return root
     }
 }
